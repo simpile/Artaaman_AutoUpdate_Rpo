9bc38306380052f538008509c62ee80d7840ab01
simpile <nasim.pilevar@gmail.com>
2022-07-05 19:34:59 +0430

    compress Css and Js files in mainpage

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,4 +1,56 @@
 /* -------------------------------------------------------------------------- */
+/*                                  PRELOADER                                 */
+/* -------------------------------------------------------------------------- */
+window.onload = function(){ document.querySelector(".preloader").style.display = "none"; }
+
+
+/* -------------------------------------------------------------------------- */
+/*                                AOS-ANIMATION                               */
+/* -------------------------------------------------------------------------- */
+AOS.init({
+  // disable: "mobile",
+  duration: 800,
+  anchorPlacement: "center-bottom",
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                SMOOTH SCROLL                               */
+/* -------------------------------------------------------------------------- */
+// $(function () {
+//   $(".nav-link").on("click", function (event) {
+//     var $anchor = $(this);
+//     $("html, body")
+//       .stop()
+//       .animate(
+//         {
+//           scrollTop: $($anchor.attr("href")).offset().top - 0,
+//         },
+//         1000
+//       );
+//     event.preventDefault();
+//   });
+// });
+
+/* -------------------------------------------------------------------------- */
+/*                                  OPENMENU                                  */
+/* -------------------------------------------------------------------------- */
+  var navbarItems = document.getElementById("myNavbar-contents");
+  var icon = document.getElementsByClassName("iconify");
+  navbarItems.style.display = "none";
+function openNav() {
+
+  if (navbarItems.style.display == "none") {
+    navbarItems.style.display = "block";
+    icon[0].style.color = "#FEBF50";
+  } else {
+    navbarItems.style.display = "none";
+    icon[0].style.color = "#FFFFFF";
+  }
+}
+
+
+
+/* -------------------------------------------------------------------------- */
 /*                                   SWIPER                                   */
 /* -------------------------------------------------------------------------- */
 
