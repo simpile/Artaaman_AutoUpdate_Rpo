933cfc5717a57298b94ab0d346c16ca2c69add9c
simpile <nasim.pilevar@gmail.com>
2022-11-02 03:57:52 +0330

    change icones CDN to SVG tag

--- public/layouts/footer.js
+++ public/layouts/footer.js
@@ -1,47 +1,54 @@
 document.write(`
 <footer  class="myFooter-wrapper">
-<section class="myFooter-container mainWrapper ">
-  <div class="myFooter-header">
-    <img src="/images/logo-grey.svg" alt="Artaman's logo">
-    <h2> <strong> خلاقیت</strong> را با ما تجربه کنید</h2>
-  </div>
-  <a  class="myFooter-copyRight" target="_blank" href="https://github.com/simpile">
-    copyright by artaman
-  </a>
-  <h5 class="myFooter-titles" >
-    <span class="iconify" data-icon="ant-design:phone-filled" data-width="25"></span>
-    اطلاعات تماس
-  </h5>
-  <h5 class="myFooter-titles"  >
-    <span  class="iconify" data-icon="ci:location" data-width="25"></span>
-    آدرس
-  </h5>
-  <div class="myFooter-phones">
-  <a href="tel:09909890646">
-  0990-9860646
-</a>
-<br>
-<a href="tel:09109465940">
-  0910-9465940
-</a>
-  </div>
-  <div class="myFooter-socialMedias">
-    <a title="تلگرام" target="_blank" href="https://t.me/artaman_co">
-      <span class="iconify" data-icon="iconoir:telegram-circled" data-width="25"></span>
+  <section class="myFooter-container mainWrapper ">
+    <div class="myFooter-header">
+      <img src="/images/logo-grey.svg" alt="Artaman's logo">
+      <h2> <strong> خلاقیت</strong> را با ما تجربه کنید</h2>
+    </div>
+    <a  class="myFooter-copyRight" target="_blank" href="https://github.com/simpile">
+      copyright by artaman
     </a>
-    <a title="اینستاگرام" target="_blank" href="https://www.instagram.com/artaman_co/">
-      <span class="iconify" data-icon="lucide:instagram" data-width="25"></span>
-    </a>
-    <a title="واتساپ" target="_blank" href="https://wa.me/989909890646">
-      <span class="iconify" data-icon="ic:twotone-whatsapp" data-width="28"></span>
-    </a>
-    <a title="ایمیل"  target="_blank" href="mailto:artaamane@gmail.com">
-      <i data-width="25" class="iconify" data-icon="ant-design:mail-outlined"></i>
-    </a>
-  </div>
-  <p class="myFooter-address" >
-    کرج، ماهدشت، شهرک صنعتی پیام، خیابان سرداران، خیابان ششم شمالی، پلاک 16
-  </p>
-</section>
-</footer>
+    <h5 class="myFooter-titles" >
+      <!-- <span class="iconify" data-icon="ant-design:phone-filled" data-width="25"></span> -->
+      <svg class="iconify" xmlns="http://www.w3.org/2000/svg" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024"><path fill="currentColor" d="M885.6 230.2L779.1 123.8a80.83 80.83 0 0 0-57.3-23.8c-21.7 0-42.1 8.5-57.4 23.8L549.8 238.4a80.83 80.83 0 0 0-23.8 57.3c0 21.7 8.5 42.1 23.8 57.4l83.8 83.8A393.82 393.82 0 0 1 553.1 553A395.34 395.34 0 0 1 437 633.8L353.2 550a80.83 80.83 0 0 0-57.3-23.8c-21.7 0-42.1 8.5-57.4 23.8L123.8 664.5a80.89 80.89 0 0 0-23.8 57.4c0 21.7 8.5 42.1 23.8 57.4l106.3 106.3c24.4 24.5 58.1 38.4 92.7 38.4c7.3 0 14.3-.6 21.2-1.8c134.8-22.2 268.5-93.9 376.4-201.7C828.2 612.8 899.8 479.2 922.3 344c6.8-41.3-6.9-83.8-36.7-113.8z"/></svg>
+      اطلاعات تماس
+    </h5>
+    <h5 class="myFooter-titles"  >
+      <!-- <span  class="iconify" data-icon="ci:location" data-width="25"></span> -->
+      <svg class="iconify" xmlns="http://www.w3.org/2000/svg" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M12 21a29.776 29.776 0 0 1-3.5-3.531C6.9 15.558 5 12.712 5 10a7 7 0 0 1 11.952-4.951A6.955 6.955 0 0 1 19 10c0 2.712-1.9 5.558-3.5 7.469A29.777 29.777 0 0 1 12 21Zm0-14a3 3 0 1 0 0 6a3 3 0 0 0 0-6Z"/></svg>
+      آدرس
+    </h5>
+    <div class="myFooter-phones">
+      <a href="tel:09909890646">
+        0990-9860646
+      </a>
+      <br>
+      <a href="tel:09109465940">
+        0910-9465940
+      </a>
+    </div>
+    <div class="myFooter-socialMedias">
+      <a title="تلگرام" target="_blank" href="https://t.me/artaman_co">
+        <!-- <span class="iconify" data-icon="iconoir:telegram-circled" data-width="25"></span> -->
+        <svg class="iconify" xmlns="http://www.w3.org/2000/svg" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M18 8L5 12.5L9.5 14M18 8l-8.5 6M18 8l-4 10.5L9.5 14"/><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Z"/></g></svg>
+      </a>
+      <a title="اینستاگرام" target="_blank" href="https://www.instagram.com/artaman_co/">
+        <!-- <span  data-icon="lucide:instagram" data-width="25"></span> -->
+        <svg class="iconify" xmlns="http://www.w3.org/2000/svg" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8A4 4 0 0 1 16 11.37zm1.5-4.87h.01"/></g></svg>
+      </a>
+      <a title="واتساپ" target="_blank" href="https://wa.me/989909890646">
+        <!-- <span class="iconify" data-icon="ic:twotone-whatsapp" data-width="28"></span> -->
+        <svg class="iconify" xmlns="http://www.w3.org/2000/svg" width="28" height="28" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M19.05 4.91A9.816 9.816 0 0 0 12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01zm-7.01 15.24c-1.48 0-2.93-.4-4.2-1.15l-.3-.18l-3.12.82l.83-3.04l-.2-.31a8.264 8.264 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24c2.2 0 4.27.86 5.82 2.42a8.183 8.183 0 0 1 2.41 5.83c.02 4.54-3.68 8.23-8.22 8.23zm4.52-6.16c-.25-.12-1.47-.72-1.69-.81c-.23-.08-.39-.12-.56.12c-.17.25-.64.81-.78.97c-.14.17-.29.19-.54.06c-.25-.12-1.05-.39-1.99-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.14-.25-.02-.38.11-.51c.11-.11.25-.29.37-.43s.17-.25.25-.41c.08-.17.04-.31-.02-.43s-.56-1.34-.76-1.84c-.2-.48-.41-.42-.56-.43h-.48c-.17 0-.43.06-.66.31c-.22.25-.86.85-.86 2.07c0 1.22.89 2.4 1.01 2.56c.12.17 1.75 2.67 4.23 3.74c.59.26 1.05.41 1.41.52c.59.19 1.13.16 1.56.1c.48-.07 1.47-.6 1.67-1.18c.21-.58.21-1.07.14-1.18s-.22-.16-.47-.28z"/></svg>
+      </a>
+      <a title="ایمیل"  target="_blank" href="mailto:artaamane@gmail.com">
+        <!-- <i data-width="25" class="iconify" data-icon="ant-design:mail-outlined"></i> -->
+        <svg class="iconify" xmlns="http://www.w3.org/2000/svg" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024"><path fill="currentColor" d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-40 110.8V792H136V270.8l-27.6-21.5l39.3-50.5l42.8 33.3h643.1l42.8-33.3l39.3 50.5l-27.7 21.5zM833.6 232L512 482L190.4 232l-42.8-33.3l-39.3 50.5l27.6 21.5l341.6 265.6a55.99 55.99 0 0 0 68.7 0L888 270.8l27.6-21.5l-39.3-50.5l-42.7 33.2z"/></svg>
+      </a>
+    </div>
+    <p class="myFooter-address" >
+      کرج، ماهدشت، شهرک صنعتی پیام، خیابان سرداران، خیابان ششم شمالی، پلاک 16
+    </p>
+  </section>
+  </footer>
+
 `)
