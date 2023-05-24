53fe6e2e91bca31ca1de414c744c3687665d14f6
simpile <nasim.pilevar@gmail.com>
2023-05-24 03:38:09 +0330

    try to fix FQA oppening problem

--- public/js/singlePage.js
+++ public/js/singlePage.js
@@ -42,3 +42,23 @@ waCurrentPage = function () {
       window.location.pathname
   );
 };
+
+// FQA
+
+          
+
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("acordActive");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+         
