39c102291b223bd95cb01a5d065c1d4cef13f7bf
simpile <nasim.pilevar@gmail.com>
2024-08-17 15:06:51 +0330

    remove extra comments

--- config/multer.js
+++ config/multer.js
@@ -1,18 +1,3 @@
-// // // multer.js
-// // const multer = require('multer');
-// // const uuid = require('uuid').v4;
-
-// // //? multer configuration
-// // const storage = multer.diskStorage({
-// //     destination: (req, file, cb) => {
-// //         cb(null, 'public/images/NEWS')
-// //     },
-// //     filename: (req, file, cb) => {
-// //         cb(null, `${uuid()}_${file.originalname}`);
-// //     }
-// // });
-// // const upload = multer({storage});
-
 
 // // multer.js
 const multer = require('multer');
