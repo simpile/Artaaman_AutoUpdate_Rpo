e0b7d2385195bdf67cd1cd68d8909a06b63413a1
simpile <nasim.pilevar@gmail.com>
2024-08-24 04:18:52 +0330

    remove extra commented codes

--- public/js/galleryPage.js
+++ public/js/galleryPage.js
@@ -22,40 +22,6 @@ var lightboxDescription = GLightbox({
      });
    }
 
-  
-  // function call(id) {
-  //   const items = Array.from(document.getElementsByClassName("filter"));
-  //   items.map(function (item, index) {
-  //     // console.log(item);
-  //     if (id === "all") {
-  //       item.classList.remove('d-none');
-  //       item.classList.add('fadeIn','glightbox');
-  //       setTimeout(clean,500);
-  //     } else {
-  //       const check = items[index].classList.contains(id);
-  //       // console.log("check:"+check);
-  
-  //       if (check) {
-  //         item.classList.remove('d-none');
-  //         item.classList.add('fadeIn','glightbox');
-  //       } else {
-  //         item.classList.add('d-none');
-  //         item.classList.remove('fadeIn','glightbox');
-  //       }
-  //     }
-  //   }
-  //   )
-  // }
-
-  
-  
-  // function clean() {
-  //   const items = Array.from(document.getElementsByClassName("filter"));
-  //   items.map(function (item, index) {
-  //     item.classList.remove('fadeIn');
-  //   })
-  // }
-  // window.onload=call('products');
   function call(id) {
     const items = Array.from(document.getElementsByClassName("filter"));
     items.forEach(function (item) {
