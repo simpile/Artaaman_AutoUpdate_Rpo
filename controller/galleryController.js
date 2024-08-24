5f3d62a54ab64c407ee3e01f3292b786b9100fd5
simpile <nasim.pilevar@gmail.com>
2024-08-24 03:53:00 +0330

    add pagination to galleryPage

--- controller/galleryController.js
+++ controller/galleryController.js
@@ -1,48 +1,5 @@
 // // galleryController 
-// const Image = require('../model/imageModel');
 
-// // Get gallery images
-// exports.getGalleryPage = async (req, res) => {
-
-//     try {
-//         const images = await Image.find();  // Ensure `Image` is the correct model
-//         // images = images.map(image => {
-//         //     image.src = image.src.replace(/\134\134/g,"/"); // Replace backslashes with forward slashes
-//         //     return image;
-//         // });
-//         console.log(images); // Debugging: Check the output to verify image paths
-//         // console.log(req.file.path);
-//         res.render('galleryPage', { images }); // Note: Ensure the view name matches the file name
-        
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send('Server Error');
-//     }
-// };
-
-// const Image = require('../model/imageModel');
-
-// // Get gallery images with pagination
-// exports.getGalleryPage = async (req, res) => {
-//     try {
-//         const page = parseInt(req.query.page) || 1;
-//         const limit = 9; // تعداد آیتم‌ها در هر صفحه
-//         const skip = (page - 1) * limit;
-
-//         const images = await Image.find().skip(skip).limit(limit);
-//         const totalImages = await Image.countDocuments();
-//         const totalPages = Math.ceil(totalImages / limit);
-
-//         res.render('galleryPage', { 
-//             images, 
-//             currentPage: page, 
-//             totalPages 
-//         });
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send('Server Error');
-//     }
-// };
 const Image = require('../model/imageModel');
 
 // Get gallery images with pagination
